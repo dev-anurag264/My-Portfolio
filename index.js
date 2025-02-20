133d3bfbc60441e73e13d8cc0799b45e88b8a1f1
Anurag Prasad <anurag@herody.in>
2025-02-20 09:37:15 +0530

    header section done

---
+++ index.js
@@ -0,0 +1,9 @@
+//alert("Welcome to my website");
+
+//set colour of an element : document.querySelector("li").style.color = "red";
+
+//document.querySelector("button").classList.add("invisible");
+//document.querySelector("button").classList.remove("invisible");
+//document.querySelector("btn btn-success btn-sm").classList.add("invisible");
+
+document.querySelector("h1").classList.add("huge");
